7848642514ff68b59c2b9767d6d2004145cce1fb
maria-guerrero <mariadol.gc@gmail.com>
2022-01-27 13:01:25 +0100

    add emblems in details

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,18 +1,28 @@
 import {Link} from 'react-router-dom';
 import GryffindorEmblem from '../images/gryffindor.png';
+import RavenclawEmblem from '../images/ravenclaw.png';
+import SlytherinEmblem from '../images/slytherin.png';
+import HufflepuffEmblem from '../images/hufflepuff.png';
 
 const CharacterDetail = (props) => {
 
     const emblemChange = () => {
-        if (props.person.house === 'gryffindor') {
+        if (props.person.house === 'Gryffindor') {
           return GryffindorEmblem;
+        } 
+        else if(props.person.house === 'Ravenclaw') {
+          return RavenclawEmblem
         }
+        else if(props.person.house=== 'Slytherin') {
+          return SlytherinEmblem;
+        }
+        else if(props.person.house === 'Hufflepuff')
+        return HufflepuffEmblem;
       }
 
     return (
         <>
         <section className="sectionDetailPage">
-        <img className="gryffindorEmblem" src={emblemChange()} alt="gryffindor" />
         <Link className="back" to="/">Back</Link>
                 <div className="divInfoDetail">
                 <img
@@ -27,6 +37,7 @@ const CharacterDetail = (props) => {
             <p>Specie: {props.person.species}</p>
             <p>Gender: {props.person.gender}</p>
             <p>Hogwarts house: {props.person.house}</p>
+            <img className="housesEmblem" src={emblemChange()} alt="Emblem" />
             </div>
             </div>
         </section>
